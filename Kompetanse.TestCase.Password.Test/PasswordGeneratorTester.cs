fd1e991393301d5fe85bc67750d41d5bdc75b7a2
Øistein Stemme <Oistein.Stemme@fhi.no>
2022-10-29 14:32:36 +0200

    Øisteins løsningsforslag

--- Kompetanse.TestCase.Password.Test/PasswordGeneratorTester.cs
+++ Kompetanse.TestCase.Password.Test/PasswordGeneratorTester.cs
@@ -1,3 +1,5 @@
+using Xunit.Abstractions;
+
 namespace Kompetanse.TestCase.Password.Test
 {
     /*
@@ -12,31 +14,105 @@ namespace Kompetanse.TestCase.Password.Test
         Skal ikke innehilde: stor-O, 0, liten L og I, og norske spesialbokstaver æ, ø og å
    */
 
-
-
-    public class PasswordGeneratorTester
+    public class PasswordGeneratorSetup : IDisposable
     {
-   
-        IPasswordGenerator passwordGenerator;
+        public int NumOfPasswords => 1000;
+        public string[] Passwords { get; }
 
-        public PasswordGeneratorTester()
+        public PasswordGeneratorSetup()
         {
-            passwordGenerator=new PasswordGenerator();
+            IPasswordGenerator passwordGenerator = new PasswordGenerator();
+
+            Passwords = new string[NumOfPasswords];
+            for (int i = 0; i < NumOfPasswords; i++)
+            {
+                Passwords[i] = passwordGenerator.Genererate();
+            }
         }
 
+        public void Dispose() { }
+    }
+
+    /// <summary>
+    /// Unit tests for the PasswordGenerator class.
+    /// Uses a class fixture for one time setup of the test class.
+    /// The fixture generates a specified number of passwords which are then all tested, ao. for uniqueness.
+    /// </summary>
+    public class PasswordGeneratorTester : IClassFixture<PasswordGeneratorSetup>
+    {
+        private readonly PasswordGeneratorSetup _classwideFixture;
+
+        private int NumOfPasswords => _classwideFixture.NumOfPasswords;
+
+        private string[] Passwords => _classwideFixture.Passwords;
+
+        public PasswordGeneratorTester(PasswordGeneratorSetup fixture)
+        {
+            _classwideFixture = fixture;
+        }
 
         [Fact]
-        [Trait("PasswordGenerator", "")]
-        public void TestIsImplemented()
+        public void TestUniqueness()
         {
-           String password=passwordGenerator.Genererate();
-            Assert.NotNull(password);
+            for (int i = 0; i < NumOfPasswords; i++)
+            {
+                for (int j = i + 1; j < NumOfPasswords; j++)
+                {
+                    Assert.True(!Passwords[i].Equals(Passwords[j]));
+                }
+            }
         }
 
-        //TODO Lage komplett sett av tester..
+        [Fact]
+        public void TestNoIllegalChars()
+        {
+            for (int i = 0; i < NumOfPasswords; i++)
+            {
+                var pwd = Passwords[i];
+                Assert.False(pwd.Contains('O'));
+                Assert.False(pwd.Contains('0'));
+                Assert.False(pwd.Contains('l'));
+                Assert.False(pwd.Contains('I'));
+                Assert.False(pwd.Contains('æ', StringComparison.InvariantCultureIgnoreCase));
+                Assert.False(pwd.Contains('ø', StringComparison.InvariantCultureIgnoreCase));
+                Assert.False(pwd.Contains('å', StringComparison.InvariantCultureIgnoreCase));
+            }
+        }
 
+        [Fact]
+        public void TestContainsSpecialChar()
+        {
+            for (int i = 0; i < NumOfPasswords; i++)
+            {
+                Assert.True(Passwords[i].IndexOfAny(PasswordGenerator.SpecialChars) > -1);
+            }
+        }
 
+        [Fact]
+        public void TestContainsDigit()
+        {
+            for (int i = 0; i < NumOfPasswords; i++)
+            {
+                Assert.True(Passwords[i].IndexOfAny(PasswordGenerator.Digits) > -1);
+            }
+        }
 
+        [Fact]
+        public void TestContainsLowerCaseChar()
+        {
+            for (int i = 0; i < NumOfPasswords; i++)
+            {
+                Assert.True(Passwords[i].IndexOfAny(PasswordGenerator.LowerCaseChars) > -1);
+            }
+        }
 
+        [Fact]
+        public void TestContainsSpecialUpperCaseChar()
+        {
+            for (int i = 0; i < NumOfPasswords; i++)
+            {
+                Assert.True(Passwords[i].IndexOfAny(PasswordGenerator.UpperCaseChars) > -1);
+            }
+        }
     }
 }
--- Kompetanse.TestCase.Password/PasswordGenerator.cs
+++ Kompetanse.TestCase.Password/PasswordGenerator.cs
@@ -14,11 +14,55 @@
 
     public class PasswordGenerator : IPasswordGenerator
     {
-        public String Genererate()
-        {
-            //TODO Implemnetere.
+        private const int PasswordLength = 16;
 
-            throw new NotImplementedException();
+        // A password must contain at least one of each of the following character categories, and no other characters than the ones listed here.
+        public static readonly char[] LowerCaseChars = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        public static readonly char[] UpperCaseChars = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        public static readonly char[] Digits = { '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        public static readonly char[] SpecialChars = { '#', '!', '?', '+', '&', '(', ')', '/' };
+
+        // Array holding all the arrays with characters
+        private readonly char[][] _allowedChars = { LowerCaseChars, UpperCaseChars, Digits, SpecialChars };
+
+        // Array with the number of characters in each category
+        private readonly int[] _numberOfCharsInCategories = { LowerCaseChars.Length, UpperCaseChars.Length, Digits.Length, SpecialChars.Length };
+
+        private readonly Random _rng = new Random((int) DateTime.Now.Ticks);
+
+        public string Genererate()
+        {
+            int numberOfCategories = _numberOfCharsInCategories.Length;
+            // Array to keep track of which categories that are represented with at least one character in the password
+            bool[] usedCategories = {false, false, false, false};
+
+            string password = "";
+
+            // Generate each character in the password randomly.
+            for (int i = 0; i < PasswordLength; i++)
+            {
+                // First, pick a category (lowercase, uppercase, digits or special chars)
+                int category = -1;
+                // On the last characters, check if we're missing some categories and make sure to include them if so
+                if (i >= PasswordLength - numberOfCategories + 1)
+                {
+                    category = Array.IndexOf(usedCategories, false);
+                }
+                if (category == -1)
+                    // Pick a category randomly
+                    category = _rng.Next(0, numberOfCategories);
+
+                // Mark category as used
+                usedCategories[category] = true;
+
+                // Pick a character randomly from the category
+                int index = _rng.Next(0, _numberOfCharsInCategories[category] - 1);
+
+                // Add the new character to the password
+                password += _allowedChars[category][index];
+            }
+
+            return password;
         }
 
     }
